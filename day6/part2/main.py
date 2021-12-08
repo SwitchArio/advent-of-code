8e1ba7f83e014a6a39942c279106a457d25e8599
SwitchArio <76228047+SwitchArio@users.noreply.github.com>
2021-12-08 16:55:58 +0100

    Create main.py

---
+++ day6/part2/main.py
@@ -0,0 +1,36 @@
+with open("input.txt") as f:
+    lines = f.readlines()
+
+initial_counters = lines[0].split(",")
+DAYS = 256
+
+all_fish = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+}
+
+for counter in initial_counters:
+    counter = int(counter)
+    all_fish[counter] += 1
+
+for day in range(DAYS):
+    next_value, saved_value = 0, 0
+    for i in range(9, -1, -1):
+        if i == 0:
+            all_fish[6] += next_value
+            all_fish[8] += next_value
+        else:
+            saved_value = all_fish[i - 1]  # save the value of the next one
+            all_fish[i - 1] = next_value  # put the value in the next one
+            next_value = saved_value  # the saved values is set to next value
+total = 0
+for i in range(9):
+    total += all_fish[i]
+print(total)
